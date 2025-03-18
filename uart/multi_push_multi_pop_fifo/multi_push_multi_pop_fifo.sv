40a32dd6340cf78babfd551959f33f892b5331f2
Fankeks <fkoleda.2012@gmail.com>
2025-03-18 12:57:52 +0300

    multi push multi pop fifo test

---
+++ uart/multi_push_multi_pop_fifo/multi_push_multi_pop_fifo.sv
@@ -0,0 +1,99 @@
+module multi_push_multi_pop_fifo #(
+    parameter W = 16,
+    parameter D = 16,
+    parameter N = 2,   // max push / pop
+    localparam WN = $clog2(N + 1)
+) (
+    input  logic                         clk,
+    input  logic                         rst,
+
+    input  logic [WN - 1 : 0]            push,
+    input  logic [ N - 1 : 0][W - 1 : 0] push_data,
+
+    input  logic [WN - 1 : 0]            pop,
+    output logic [ N - 1 : 0][W - 1 : 0] pop_data,
+
+    output logic [WN - 1 : 0]            can_push,
+    output logic [WN - 1 : 0]            can_pop
+);
+
+    //------------------------------------------------------------------------
+
+    localparam pointer_width = $clog2 (D) + 1,
+                          counter_width = $clog2 (D + 1) + 1;
+
+    localparam [counter_width - 1:0] max_ptr = counter_width' (D - 1);
+
+    //------------------------------------------------------------------------
+
+    logic [pointer_width - 1:0] wr_ptr, rd_ptr;
+    logic wr_ptr_odd_circle, rd_ptr_odd_circle;
+
+    logic [D - 1 : 0] [W - 1:0] data;
+
+    //------------------------------------------------------------------------
+
+    always_ff @ (posedge clk or posedge rst)
+        if (rst)
+        begin
+            wr_ptr <= '0;
+            wr_ptr_odd_circle <= 1'b0;
+        end
+        else if (|push)
+        begin
+            if (wr_ptr + push > max_ptr)
+            begin
+                wr_ptr <= wr_ptr + push - max_ptr - 1;
+                wr_ptr_odd_circle <= ~ wr_ptr_odd_circle;
+            end
+            else
+            begin
+                wr_ptr <= wr_ptr + push;
+            end
+        end
+
+    //------------------------------------------------------------------------
+
+    always_ff @ (posedge clk or posedge rst)
+        if (rst)
+        begin
+            rd_ptr <= '0;
+            rd_ptr_odd_circle <= 1'b0;
+        end
+        else if (|pop)
+        begin
+            if (rd_ptr + pop > max_ptr)
+            begin
+                rd_ptr <= rd_ptr + pop - max_ptr - 1;
+                rd_ptr_odd_circle <= ~ rd_ptr_odd_circle;
+            end
+            else
+            begin
+                rd_ptr <= rd_ptr + pop;
+            end
+        end
+
+    //------------------------------------------------------------------------
+    // output
+
+    always_ff @(posedge clk) begin
+        if (|push) begin
+            for (int i = 0; i < N; i++) begin
+                if (i < push) begin
+                    data[(i + wr_ptr > max_ptr) ? i + wr_ptr - max_ptr - 1 : i + wr_ptr] <= push_data[i];
+                end
+            end
+        end
+    end
+
+    always_comb begin
+        for (int j = 0; j < N; j++) begin
+            pop_data[j] = data[(j + rd_ptr > max_ptr) ? j + rd_ptr - max_ptr - 1 : j + rd_ptr];
+        end
+    end
+
+    // // can_push / can_pop
+    assign can_push = (rd_ptr_odd_circle == wr_ptr_odd_circle) ? D - (wr_ptr - rd_ptr) : (rd_ptr - wr_ptr);
+    assign can_pop  = (rd_ptr_odd_circle == wr_ptr_odd_circle) ? wr_ptr - rd_ptr : D - (rd_ptr - wr_ptr);
+    
+endmodule
---
+++ uart/multi_push_multi_pop_fifo/testbench.sv
@@ -0,0 +1,142 @@
+`timescale 1ns/1ps
+
+module testbench;
+    // Подключение
+    localparam W = 8;
+    localparam D = 4;
+    localparam N = 4;
+    localparam WN = $clog2(N + 1);
+
+    logic                clk;
+    logic                rst;
+
+    logic [WN - 1 : 0] push;
+    logic [N - 1 : 0][W - 1 : 0] push_data;
+
+    logic [WN - 1 : 0] pop;
+    logic [N - 1 : 0][W - 1 : 0] pop_data;
+
+    logic [WN - 1 : 0] can_push;
+    logic [WN - 1 : 0] can_pop;
+
+    multi_push_multi_pop_fifo #(
+    .W (W),
+    .D (D),
+    .N (N)   // max push / pop
+    ) node (
+        .clk(clk),
+        .rst(rst),
+
+        .push(push),
+        .push_data(push_data),
+
+        .pop(pop),
+        .pop_data(pop_data),
+
+        .can_push(can_push),
+        .can_pop(can_pop)
+    );
+
+    initial begin
+        $dumpfile("fifo_multi.vcd");
+        $dumpvars;
+    end
+    // Запись значений в веса
+
+    // Генерация clk
+    parameter CLK_PERIOD = 20; // 50 МГц
+    initial begin
+        clk <= 0;
+        forever begin
+            #(CLK_PERIOD / 2); clk <= ~clk;
+        end
+    end
+
+    //Генерация rst
+    initial begin
+        rst <= 'b1;
+        @(posedge clk);
+        @(posedge clk);
+        @(posedge clk);
+        rst <= 'b0;
+    end
+
+    // Генерация входных сигналов
+    initial begin
+        pop <= 'b0;
+        wait(rst);
+        @(posedge clk);
+        @(posedge clk);
+        @(posedge clk);
+        @(posedge clk);
+
+        push <= 'd3;
+        push_data[0] <= 'd1;
+        push_data[1] <= 'd2;
+        push_data[2] <= 'd3;
+        push_data[3] <= 'd4;
+
+        @(posedge clk);
+        push <='d0;
+        pop <= 'd1;
+
+        @(posedge clk);
+        pop <= 'd1;
+
+        @(posedge clk);
+        pop <= 'd1;
+
+        @(posedge clk);
+        pop <= 'd0;
+
+        @(posedge clk);
+        pop <= 'd0;
+        push <= 'd3;
+        push_data[0] <= 'd6;
+        push_data[1] <= 'd7;
+        push_data[2] <= 'd8;
+        push_data[3] <= 'd9;
+
+        @(posedge clk);
+        push <= 'd0;
+    //---------------------------------------------------------------------------------------------
+        //1
+        @(posedge clk);
+        repeat (20) begin
+            @(posedge clk);
+        end
+        $finish;
+    end
+    
+    // Проверка
+    // initial begin
+    //     wait(~arstn);
+    //     @(posedge clk);
+    //     @(posedge clk);
+    // //---------------------------------------------------------------------------------------------
+    //     repeat (32)
+    //     begin
+    //         @(posedge clk);
+    //         if ($signed(psumm_o) != ($signed(psumm_i) + $signed(weight_i) * $signed(x_i) )) begin
+    //             $display($signed(psumm_o));
+    //             $display(($signed(psumm_i) + $signed(weight_i) * $signed(x_i)));
+    //             $display($signed(psumm_i));
+    //             $display($signed(weight_i));
+    //             $display($signed(x_i));
+    //             $error("BAD");
+    //         end
+    //         else begin
+    //             $display("PASS");
+    //             // $display($signed(psumm_o));
+    //             // $display($signed(psumm_i));
+    //             // $display($signed(weight_i));
+    //             // $display($signed(x_i));
+    //         end
+    //     end
+    // //---------------------------------------------------------------------------------------------
+    //     @(posedge clk);
+    //     @(posedge clk);
+    //     @(posedge clk);
+    //     $finish;
+    // end
+endmodule
